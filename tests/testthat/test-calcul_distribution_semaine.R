c2e05d8e63588a3cda9b5ef682de07643324efcc
ainarampx <ainaramartinez2426@gmail.com>
2026-04-13 11:13:53 +0200

    question 19

--- tests/testthat/test-calcul_distribution_semaine.R
+++ tests/testthat/test-calcul_distribution_semaine.R
@@ -5,9 +5,35 @@ test_that("calcul_distribution_semaine calcule les trajets par jour de la semain
     Total = c(10, 20, 30, 40)
   )
 
-  res <- calcul_distribution_semaine(df_test)
+  res <- calcul_distribution_semaine(df_test, filtre = FALSE)
 
   expect_true("trajets" %in% names(res))
   expect_true(any(res$`Jour de la semaine` == 1))
   expect_equal(res$trajets[res$`Jour de la semaine` == 1], 30)
+})
+
+test_that("calcul_distribution_semaine ne filtre pas les données si filtre = FALSE", {
+  df_test <- data.frame(
+    check.names = FALSE,
+    `Jour de la semaine` = c(1, 1, 2),
+    Total = c(10, 20, 99999),
+    `Probabilité de présence d'anomalies` = c(NA, NA, "Faible")
+  )
+
+  res <- calcul_distribution_semaine(df_test, filtre = FALSE)
+
+  expect_equal(res$trajets[res$`Jour de la semaine` == 2], 99999)
+})
+
+test_that("calcul_distribution_semaine filtre les anomalies si filtre = TRUE", {
+  df_test <- data.frame(
+    check.names = FALSE,
+    `Jour de la semaine` = c(1, 1, 2),
+    Total = c(10, 20, 99999),
+    `Probabilité de présence d'anomalies` = c(NA, NA, "Faible")
+  )
+
+  res <- calcul_distribution_semaine(df_test, filtre = TRUE)
+
+  expect_false(any(res$trajets == 99999))
 })
